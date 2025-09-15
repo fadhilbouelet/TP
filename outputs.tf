24ead1e118ce7f0ef73e1a0353f86f8fe510fd68
fadhil <161585269+fadhilbouelet@users.noreply.github.com>
2025-09-15 14:57:43 +0200

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,13 @@ output "public_dns" {
   description = "Le FQDN (nom DNS) de la VM"
   value       = azurerm_public_ip.main.fqdn
 }
+
+output "storage_account_name" {
+  description = "Nom du Storage Account"
+  value       = azurerm_storage_account.main.name
+}
+
+output "storage_container_name" {
+  description = "Nom du Storage Container"
+  value   
+

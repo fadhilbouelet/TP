549eadb2a11ede7dfdb6eef97600f78bea8ed4a9
fadhil <161585269+fadhilbouelet@users.noreply.github.com>
2025-09-15 17:20:37 +0200

    Add files via upload

---
+++ monitoring.tf
@@ -0,0 +1,58 @@
+# monitoring.tf
+
+resource "azurerm_monitor_action_group" "main" {
+  name                = "ag-${var.resource_group_name}-alerts"
+  resource_group_name = azurerm_resource_group.main.name
+  short_name          = "vm-alerts"
+
+  email_receiver {
+    name          = "admin"
+    email_address = var.alert_email_address
+     use_common_alert_schema = true
+  }
+}
+
+# CPU Metric Alert (using platform metrics)
+resource "azurerm_monitor_metric_alert" "cpu_alert" {
+  name                = "cpu-alert-${azurerm_linux_virtual_machine.main.name}"
+  resource_group_name = azurerm_resource_group.main.name
+  scopes              = [azurerm_linux_virtual_machine.main.id]
+  description         = "Alert when CPU usage exceeds 70%"
+  severity            = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 70
+  }
+
+  window_size   = "PT5M"
+  frequency     = "PT1M"
+  auto_mitigate = true
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+  }
+}
+
+# Alerte RAM
+resource "azurerm_monitor_metric_alert" "memory" {
+  name                = "vm-memory-alert"
+  resource_group_name = azurerm_resource_group.main.name
+  scopes              = [azurerm_linux_virtual_machine.main.id]
+  description         = "Alerte si la RAM dispo < 512Mo"
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Available Memory Bytes"
+    aggregation      = "Average"
+    operator         = "LessThan"
+    threshold        = 536870912  # 512MB en bytes
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+  }
+}
